2b57126969a90a0ee9c45a34c8234b26880218d4
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2024-09-07 17:05:11 +0100

    Move test to the correct location

--- application/src/test/kotlin/com/tamj0rd2/skullking/domain/service/ViewPlayerGameStateServiceTest.kt
+++ application/src/test/kotlin/com/tamj0rd2/skullking/domain/service/ViewPlayerGameStateServiceTest.kt
@@ -1,7 +1,8 @@
-package com.tamj0rd2.skullking.port.input
+package com.tamj0rd2.skullking.domain.service
 
 import com.tamj0rd2.skullking.ApplicationDomainDriver
 import com.tamj0rd2.skullking.ApplicationDriver
+import com.tamj0rd2.skullking.port.input.ViewPlayerGameStateUseCaseContract
 import com.tamj0rd2.skullking.port.output.GameEventsInMemoryAdapter
 
 class ViewPlayerGameStateServiceTest : ViewPlayerGameStateUseCaseContract() {
