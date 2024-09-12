def5676de052a00e7b79064e7d47d7f552e9d216
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2024-09-12 22:07:15 +0100

    s - make Game a class and allow it to have mutable state

--- application/src/main/kotlin/com/tamj0rd2/skullking/domain/model/Game.kt
+++ application/src/main/kotlin/com/tamj0rd2/skullking/domain/model/Game.kt
@@ -4,6 +4,7 @@ import com.tamj0rd2.skullking.port.output.GameEventsPort
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result4k
 import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.onFailure
 import dev.forkhandles.values.UUIDValueFactory
 import dev.forkhandles.values.Value
 import java.util.UUID
@@ -16,20 +17,27 @@ value class GameId private constructor(
 }
 
 @Suppress("CONTEXT_RECEIVERS_DEPRECATED") // When contextParameters are available, I'll migrate.
-@ConsistentCopyVisibility
-data class Game private constructor(
+class Game private constructor(
     val id: GameId,
-    val players: List<PlayerId> = emptyList(),
 ) {
+    private val _players = mutableListOf<PlayerId>()
+    val players get() = _players.toList()
+
+    fun addPlayer(playerId: PlayerId): Result4k<Unit, AddPlayerErrorCode> {
+        if (players.size >= MAXIMUM_PLAYER_COUNT) return Failure(GameIsFull)
+        _players.add(playerId)
+        return Success(Unit)
+    }
+
     companion object {
         const val MAXIMUM_PLAYER_COUNT = 6
 
         context(GameEventsPort)
         internal fun load(id: GameId): Game {
             val eventsForThisGame = findGameEvents(id)
-            return eventsForThisGame.fold(new(id)) { game, event ->
+            return eventsForThisGame.fold(Game(id)) { game, event ->
                 when (event) {
-                    is PlayerJoined -> game.copy(players = game.players + event.playerId)
+                    is PlayerJoined -> game.apply { addPlayer(event.playerId) }
                 }
             }
         }
@@ -39,17 +47,10 @@ data class Game private constructor(
             gameId: GameId,
             playerId: PlayerId,
         ): Result4k<Unit, AddPlayerErrorCode> {
-            val game = load(gameId)
-            if (game.players.size >= MAXIMUM_PLAYER_COUNT) return Failure(GameIsFull)
+            load(gameId).addPlayer(playerId).onFailure { return it }
             saveGameEvents(listOf(PlayerJoined(gameId, playerId)))
             return Success(Unit)
         }
-
-        private fun new(id: GameId): Game =
-            Game(
-                id = id,
-                players = emptyList(),
-            )
     }
 }
 
