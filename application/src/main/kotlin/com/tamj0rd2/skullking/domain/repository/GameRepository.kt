af4d447ebfb6099f917a43f0eb4a9e536b7f1dd1
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2024-09-14 12:30:27 +0100

    b - move logic of saving/loading to GameRepository

--- application/src/main/kotlin/com/tamj0rd2/skullking/domain/repository/GameRepository.kt
+++ application/src/main/kotlin/com/tamj0rd2/skullking/domain/repository/GameRepository.kt
@@ -7,9 +7,7 @@ import com.tamj0rd2.skullking.port.output.GameEventsPort
 class GameRepository(
     private val gameEventsPort: GameEventsPort,
 ) {
-    fun load(gameId: GameId): Game {
-        return Game(gameId, gameEventsPort.findGameEvents(gameId))
-    }
+    fun load(gameId: GameId): Game = Game(gameId, gameEventsPort.findGameEvents(gameId))
 
     fun save(game: Game) {
         gameEventsPort.saveGameEvents(game.changes)
