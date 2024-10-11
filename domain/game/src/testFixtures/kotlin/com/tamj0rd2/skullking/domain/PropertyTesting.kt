df8c89cbc8e960a6063f748185fbb23e7fa64ca9
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2024-10-11 21:52:31 +0100

    s - make sure to check for errors in GameAction

--- domain/game/src/testFixtures/kotlin/com/tamj0rd2/skullking/domain/PropertyTesting.kt
+++ domain/game/src/testFixtures/kotlin/com/tamj0rd2/skullking/domain/PropertyTesting.kt
@@ -3,13 +3,10 @@ package com.tamj0rd2.skullking.domain
 import com.tamj0rd2.skullking.domain.model.PlayerId
 import com.tamj0rd2.skullking.domain.model.game.Game
 import com.tamj0rd2.skullking.domain.model.game.Game.Companion.MAXIMUM_PLAYER_COUNT
-import com.tamj0rd2.skullking.domain.model.game.GameCreated
 import com.tamj0rd2.skullking.domain.model.game.GameErrorCode
-import com.tamj0rd2.skullking.domain.model.game.GameId
-import com.tamj0rd2.skullking.domain.model.game.PlayerJoined
 import dev.forkhandles.result4k.Result4k
 import dev.forkhandles.result4k.Success
-import dev.forkhandles.result4k.onFailure
+import dev.forkhandles.result4k.orThrow
 import io.kotest.common.runBlocking
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.arbitrary
@@ -21,22 +18,7 @@ import strikt.assertions.isA
 
 // NOTE: these arbs need to be kept up to date, as/when I add more actions and events.
 object GameArbs {
-    private val gameIdArb = Arb.uuid(allowNilValue = false).map { GameId.of(it) }
     val playerIdArb = Arb.uuid().map { PlayerId.of(it) }
-
-    private fun gameCreatedArb(gameId: GameId) = arbitrary { GameCreated(gameId) }
-
-    private fun playerJoinedArb(gameId: GameId) = playerIdArb.map { playerId -> PlayerJoined(gameId, playerId) }
-
-    val validGameEventsArb =
-        arbitrary {
-            val gameId = gameIdArb.bind()
-
-            buildList {
-                add(gameCreatedArb(gameId).bind())
-                addAll(Arb.set(playerJoinedArb(gameId), 0..MAXIMUM_PLAYER_COUNT).bind())
-            }
-        }
 
     private val playerJoinedActionArb =
         arbitrary {
@@ -72,10 +54,12 @@ data class GameActions(
 
     fun applyAllTo(game: Game) {
         actions.fold(listOf<GameAction>()) { appliedActions, action ->
-            action.mutate(game).onFailure {
+            try {
+                action.mutate(game)
+            } catch (e: GameErrorCode) {
                 println("Previously applied actions:\n${appliedActions.joinToString("\n")}\n")
                 println("Failed to apply: $action")
-                throw it.reason
+                throw e
             }
 
             appliedActions + action
@@ -91,7 +75,7 @@ data class GameAction(
 ) {
     override fun toString(): String = description
 
-    fun mutate(game: Game) = mutation(game)
+    fun mutate(game: Game) = mutation(game).orThrow()
 
     override fun equals(other: Any?): Boolean {
         if (other !is GameAction) return false
