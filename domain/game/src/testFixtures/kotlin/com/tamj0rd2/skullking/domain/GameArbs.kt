c3097b6d8dedf3dfbb8eedffbc754e1d93f166d0
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2024-10-20 13:32:48 +0100

    s - move dead code

--- domain/game/src/testFixtures/kotlin/com/tamj0rd2/skullking/domain/GameArbs.kt
+++ domain/game/src/testFixtures/kotlin/com/tamj0rd2/skullking/domain/GameArbs.kt
@@ -1,12 +1,9 @@
 package com.tamj0rd2.skullking.domain
 
-import com.tamj0rd2.skullking.domain.GameActionArbs.gameActionsArb
 import com.tamj0rd2.skullking.domain.model.PlayerId
-import com.tamj0rd2.skullking.domain.model.game.Game
 import com.tamj0rd2.skullking.domain.model.game.GameId
 import dev.forkhandles.result4k.Result4k
 import dev.forkhandles.result4k.Success
-import dev.forkhandles.result4k.map
 import io.kotest.common.runBlocking
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.filterIsInstance
@@ -16,13 +13,6 @@ import io.kotest.property.arbitrary.uuid
 object GameArbs {
     val playerIdArb = Arb.uuid(allowNilValue = false).map { PlayerId.of(it) }
     val gameIdArb = Arb.uuid(allowNilValue = false).map { GameId.of(it) }
-
-    val gameArb =
-        gameActionsArb
-            .map { actions ->
-                val game = Game.new()
-                game.execute(*actions.toTypedArray()).map { game }
-            }.ignoreFailures()
 }
 
 fun <T, E : Throwable> Arb<Result4k<T, E>>.ignoreFailures() = filterIsInstance<Success<T>>().map { it.value }
