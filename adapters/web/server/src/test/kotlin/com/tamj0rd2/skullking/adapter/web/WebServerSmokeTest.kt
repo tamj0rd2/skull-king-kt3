f81655f74b32dfa5eb8f1aac685b8eab3b9b3290
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2024-12-17 16:50:47 +0000

    s - remove a todo

--- adapters/web/server/src/test/kotlin/com/tamj0rd2/skullking/adapter/web/WebServerSmokeTest.kt
+++ adapters/web/server/src/test/kotlin/com/tamj0rd2/skullking/adapter/web/WebServerSmokeTest.kt
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD
 class WebServerSmokeTest {
     @Test
     fun `can start a 2 player game`() {
-        // TODO: this should test Main instead. Figure out how to do it.
         WebServer(port = Main.DEFAULT_PORT).start().use {
             val baseUri = Uri.of("ws://localhost:${it.port()}")
 
