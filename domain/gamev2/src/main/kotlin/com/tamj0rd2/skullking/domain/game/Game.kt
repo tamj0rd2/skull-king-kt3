e4c2bd49062fa47d3d2842595e5741fb5e72d963
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-03-15 22:08:06 +0000

    s - deal cards to each player

--- domain/gamev2/src/main/kotlin/com/tamj0rd2/skullking/domain/game/Game.kt
+++ domain/gamev2/src/main/kotlin/com/tamj0rd2/skullking/domain/game/Game.kt
@@ -1,14 +1,26 @@
 package com.tamj0rd2.skullking.domain.game
 
+import com.tamj0rd2.extensions.asFailure
+import com.tamj0rd2.extensions.asSuccess
+import com.tamj0rd2.skullking.domain.game.GameCommand.StartRound
 import com.tamj0rd2.skullking.domain.game.GameErrorCode.NotEnoughPlayersToStartGame
+import com.tamj0rd2.skullking.domain.game.GameErrorCode.NotImplemented
+import com.tamj0rd2.skullking.domain.game.GameEvent.BidPlaced
+import com.tamj0rd2.skullking.domain.game.GameEvent.CardPlayed
+import com.tamj0rd2.skullking.domain.game.GameEvent.GameCompleted
 import com.tamj0rd2.skullking.domain.game.GameEvent.GameStarted
+import com.tamj0rd2.skullking.domain.game.GameEvent.RoundCompleted
+import com.tamj0rd2.skullking.domain.game.GameEvent.RoundStarted
+import com.tamj0rd2.skullking.domain.game.GameEvent.TrickCompleted
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.onFailure
+import dev.forkhandles.result4k.orThrow
+import dev.forkhandles.values.IntValueFactory
 import dev.forkhandles.values.LongValueFactory
 import dev.forkhandles.values.UUIDValueFactory
 import dev.forkhandles.values.Value
 import dev.forkhandles.values.random
 import java.util.UUID
-
-sealed class GameInProgressCommand
 
 data class GameId private constructor(
     override val value: UUID,
@@ -30,15 +42,32 @@ data class Version private constructor(
     }
 }
 
+data class RoundNumber private constructor(
+    override val value: Int,
+) : Value<Int> {
+    val next: RoundNumber get() = RoundNumber(value + 1)
+
+    companion object : IntValueFactory<RoundNumber>(::RoundNumber) {
+        val none = RoundNumber(0)
+    }
+}
+
+sealed interface Card
+
+data object CannedCard : Card
+
 sealed interface GameEvent {
     val gameId: GameId
 
     data class GameStarted(
         override val gameId: GameId,
+        val players: Set<PlayerId>,
     ) : GameEvent
 
     data class RoundStarted(
         override val gameId: GameId,
+        val roundNumber: RoundNumber,
+        val dealtCards: CardsPerPlayer,
     ) : GameEvent
 
     data class BidPlaced(
@@ -62,29 +91,94 @@ sealed interface GameEvent {
     ) : GameEvent
 }
 
-class Game(
-    private val players: List<PlayerId>,
-    val events: MutableList<GameEvent>,
+sealed interface GameCommand {
+    data class StartRound(
+        val roundNumber: RoundNumber,
+    ) : GameCommand
+}
+
+class Game private constructor(
+    private val gameId: GameId,
 ) {
-    init {
-        if (players.size < 2) throw NotEnoughPlayersToStartGame()
+    val events = mutableListOf<GameEvent>()
+
+    constructor(players: Set<PlayerId>) : this(
+        gameId = GameId.random(),
+    ) {
+        appendEvent(GameStarted(gameId = gameId, players = players)).orThrow()
     }
 
-    val gameId = events.single { it is GameStarted }.gameId
+    var state = GameState.new
+        private set
 
-    constructor(players: List<PlayerId>) : this(
-        players = players,
-        events = mutableListOf(GameStarted(GameId.random())),
-    )
+    fun execute(command: GameCommand): Result4k<Unit, GameErrorCode> =
+        when (command) {
+            is StartRound ->
+                appendEvent(
+                    RoundStarted(
+                        gameId = gameId,
+                        roundNumber = state.roundNumber.next,
+                        dealtCards = CardsPerPlayer(state.players.associateWith { setOf(CannedCard) }),
+                    ),
+                )
+        }
 
-    init {
-        events.forEach(::appendEvent)
+    private fun appendEvent(event: GameEvent): Result4k<Unit, GameErrorCode> {
+        check(event.gameId == gameId) { "GameId mismatch" }
+        state = state.applyEvent(event).onFailure { return it }
+        events.add(event)
+        return Unit.asSuccess()
+    }
+}
+
+data class CardsPerPlayer(
+    val perPlayer: Map<PlayerId, Set<Card>>,
+)
+
+data class GameState(
+    val players: Set<PlayerId>,
+    val roundNumber: RoundNumber,
+    val cardsPerPlayer: CardsPerPlayer,
+) {
+    fun applyEvent(event: GameEvent): Result4k<GameState, GameErrorCode> {
+        return when (event) {
+            is BidPlaced -> {
+                NotImplemented().asFailure()
+            }
+            is CardPlayed -> {
+                NotImplemented().asFailure()
+            }
+            is GameCompleted -> {
+                NotImplemented().asFailure()
+            }
+            is GameStarted -> {
+                if (event.players.size < 2) return NotEnoughPlayersToStartGame().asFailure()
+                return copy(players = event.players).asSuccess()
+            }
+            is RoundCompleted -> {
+                NotImplemented().asFailure()
+            }
+            is RoundStarted -> {
+                return copy(cardsPerPlayer = event.dealtCards).asSuccess()
+            }
+            is TrickCompleted -> {
+                NotImplemented().asFailure()
+            }
+        }
     }
 
-    private fun appendEvent(event: GameEvent) {
+    companion object {
+        val new =
+            GameState(
+                players = emptySet(),
+                roundNumber = RoundNumber.none,
+                cardsPerPlayer = CardsPerPlayer(emptyMap()),
+            )
     }
 }
 
 sealed class GameErrorCode : IllegalStateException() {
     class NotEnoughPlayersToStartGame : GameErrorCode()
+
+    class NotImplemented : GameErrorCode()
 }
--- domain/gamev2/src/test/kotlin/com/tamj0rd2/skullking/domain/game/GameTest.kt
+++ domain/gamev2/src/test/kotlin/com/tamj0rd2/skullking/domain/game/GameTest.kt
@@ -1,37 +1,70 @@
 package com.tamj0rd2.skullking.domain.game
 
 import com.tamj0rd2.skullking.domain.game.GameErrorCode.NotEnoughPlayersToStartGame
+import com.tamj0rd2.skullking.domain.game.GameEvent.RoundStarted
+import dev.forkhandles.result4k.orThrow
 import dev.forkhandles.values.random
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.api.expectThrows
+import strikt.assertions.all
 import strikt.assertions.first
+import strikt.assertions.hasSize
 import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotEmpty
 
 class GameTest {
     companion object {
-        val atLeast2Players = listOf(PlayerId.random(), PlayerId.random())
+        val somePlayers = setOf(PlayerId.random(), PlayerId.random())
+
+        fun Game.mustExecute(command: GameCommand) = execute(command).orThrow()
     }
 
     @Nested
     inner class StartingAGame {
         @Test
         fun `a new game starts with a GameStarted event`() {
-            val game = Game(atLeast2Players)
-            expectThat(game.events).first().isA<GameEvent.GameStarted>()
+            val game = Game(somePlayers)
+            expectThat(game.events)
+                .first()
+                .isA<GameEvent.GameStarted>()
+                .get { players }
+                .isEqualTo(somePlayers)
         }
 
         @Test
         fun `cannot start a game without players`() {
-            val players = emptyList<PlayerId>()
+            val players = emptySet<PlayerId>()
             expectThrows<NotEnoughPlayersToStartGame> { Game(players) }
         }
 
         @Test
         fun `cannot start a game with a single player`() {
-            val players = listOf(PlayerId.random())
+            val players = setOf(PlayerId.random())
             expectThrows<NotEnoughPlayersToStartGame> { Game(players) }
+        }
+    }
+
+    @Nested
+    inner class StartingARound {
+        @Test
+        fun `when a round has started, each player is dealt 1 card, multiplied by the round number`() {
+            val game = Game(somePlayers)
+            game.mustExecute(GameCommand.StartRound(RoundNumber.of(1)))
+
+            val roundStartedEvent = game.events.filterIsInstance<RoundStarted>().single()
+
+            expectThat(roundStartedEvent) {
+                get { roundNumber }.isEqualTo(RoundNumber.of(1))
+                get { dealtCards.perPlayer.values }
+                    .describedAs("cards dealt to each player")
+                    .isNotEmpty()
+                    .all { hasSize(roundStartedEvent.roundNumber.value) }
+            }
+
+            expectThat(game.state.cardsPerPlayer).isEqualTo(roundStartedEvent.dealtCards)
         }
     }
 }
