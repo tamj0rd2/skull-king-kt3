c48c4bd2a018e1860b04429831a4b8e10fe3117c
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-03-16 14:29:55 +0000

    s - scaffold tests for game state

---
+++ domain/gamev2/src/test/kotlin/com/tamj0rd2/skullking/domain/game/GameStateTest.kt
@@ -0,0 +1,29 @@
+package com.tamj0rd2.skullking.domain.game
+
+import org.junit.jupiter.api.Nested
+
+class GameStateTest {
+    @Nested
+    inner class GameStartedTests
+
+    @Nested
+    inner class RoundStartedTests
+
+    @Nested
+    inner class BidPlacedTests
+
+    @Nested
+    inner class TrickStartedTests
+
+    @Nested
+    inner class CardPlayedTests
+
+    @Nested
+    inner class TrickCompletedTests
+
+    @Nested
+    inner class RoundCompletedTests
+
+    @Nested
+    inner class GameCompletedTests
+}
