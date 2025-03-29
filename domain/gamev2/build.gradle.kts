1385853fed23898aebdb4b5c392ee938c6b85655
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-03-29 19:41:31 +0000

    add power assert

--- domain/gamev2/build.gradle.kts
+++ domain/gamev2/build.gradle.kts
@@ -1,9 +1,19 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
     id("buildlogic.kotlin-library-conventions")
+    alias(libs.plugins.powerAssert)
+}
+
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
+powerAssert {
+    functions = listOf("kotlin.assert")
+    includedSourceSets = listOf("main", "test", "testFixtures")
 }
 
 dependencies {
     api(libs.values4k)
     api(libs.result4k)
     api(project(":lib:forkhandles-extensions"))
+    testImplementation(kotlin("test"))
 }
