1708b9aa699cc5da224eda93e752e61026a66450
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-03-30 21:31:13 +0100

    write some properties around emitting events and changing state

--- domain/gamev2/src/test/kotlin/com/tamj0rd2/skullking/domain/game/InvariantsTest.kt
+++ domain/gamev2/src/test/kotlin/com/tamj0rd2/skullking/domain/game/InvariantsTest.kt
@@ -7,6 +7,8 @@ import com.tamj0rd2.skullking.domain.game.PropertyTesting.testInvariantHoldsWhen
 import dev.forkhandles.result4k.Success
 import dev.forkhandles.result4k.failureOrNull
 import dev.forkhandles.result4k.orThrow
+import dev.forkhandles.result4k.peek
+import dev.forkhandles.result4k.peekFailure
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.choice
 import io.kotest.property.assume
@@ -54,6 +56,70 @@ class InvariantsTest {
 
             gameResult.orThrow().testInvariantHoldsWhenExecuting(gameCommands) { game ->
                 assertEquals(initialPlayers, game.state.players)
+            }
+        }
+    }
+
+    @Test
+    fun `every event in the game is related to that specific game`() {
+        gameInvariant(validPlayerIdsArb) { initialPlayers, gameCommands ->
+            val initialGame = Game.new(initialPlayers).orThrow()
+            val initialGameId = initialGame.id
+
+            initialGame.testInvariantHoldsWhenExecuting(gameCommands) { game ->
+                val eventGameIds =
+                    game.state.events
+                        .map { it.gameId }
+                        .toSet()
+                assert(eventGameIds.single() == initialGameId)
+            }
+        }
+    }
+
+    @Test
+    fun `each successful command results in 1 event being emitted`() {
+        gameInvariant(validPlayerIdsArb) { initialPlayers, gameCommands ->
+            val game = Game.new(initialPlayers).orThrow()
+
+            gameCommands.forEach { command ->
+                val eventsBeforeCommand = game.state.events
+
+                game.execute(command).peek {
+                    val eventsAfterCommand = game.state.events
+                    assertEquals(eventsBeforeCommand, eventsAfterCommand.dropLast(1))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `a failed command does not append any events`() {
+        gameInvariant(validPlayerIdsArb) { initialPlayers, gameCommands ->
+            val game = Game.new(initialPlayers).orThrow()
+
+            gameCommands.forEach { command ->
+                val eventsBeforeCommand = game.state.events
+
+                game.execute(command).peekFailure {
+                    val eventsAfterCommand = game.state.events
+                    assertEquals(eventsBeforeCommand, eventsAfterCommand)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `a failed command does not modify the game's state`() {
+        gameInvariant(validPlayerIdsArb) { initialPlayers, gameCommands ->
+            val game = Game.new(initialPlayers).orThrow()
+
+            gameCommands.forEach { command ->
+                val stateBeforeCommand = game.state
+
+                game.execute(command).peekFailure {
+                    val stateAfterCommand = game.state
+                    assertEquals(stateBeforeCommand, stateAfterCommand)
+                }
             }
         }
     }
