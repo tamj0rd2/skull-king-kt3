421b1c669bcfb85e0392d60bb8f492ff932009a4
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-05-27 21:15:43 +0100

    add tiny types for bid, round number and trick

---
+++ domain/gamev3/src/main/kotlin/com/tamj0rd2/skullking/domain/gamev3/RoundNumber.kt
@@ -0,0 +1,14 @@
+package com.tamj0rd2.skullking.domain.gamev3
+
+enum class RoundNumber {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+}
---
+++ domain/gamev3/src/main/kotlin/com/tamj0rd2/skullking/domain/gamev3/TrickNumber.kt
@@ -0,0 +1,14 @@
+package com.tamj0rd2.skullking.domain.gamev3
+
+enum class TrickNumber {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+}
