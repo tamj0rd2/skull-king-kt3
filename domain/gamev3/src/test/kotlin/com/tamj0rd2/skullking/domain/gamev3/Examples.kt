69cdef1a8e246fa35ff85421dbbdfdb3c5a8fed0
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-06-07 21:24:36 +0100

    Update tests to use flatmap and peek, rather than orThrow, run and apply

--- domain/gamev3/src/test/kotlin/com/tamj0rd2/skullking/domain/gamev3/Examples.kt
+++ domain/gamev3/src/test/kotlin/com/tamj0rd2/skullking/domain/gamev3/Examples.kt
@@ -1,6 +1,8 @@
 package com.tamj0rd2.skullking.domain.gamev3
 
+import dev.forkhandles.result4k.flatMap
 import dev.forkhandles.result4k.orThrow
+import dev.forkhandles.result4k.peek
 import dev.forkhandles.values.random
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
@@ -16,21 +18,20 @@ class Examples {
 
         Game
             .new(setOf(player1, player2))
-            .orThrow()
-            .run { execute(StartRoundCommand).orThrow() }
-            .run { execute(PlaceBidCommand(player1, SomeBid.One)).orThrow() }
-            .run { execute(PlaceBidCommand(player2, SomeBid.Zero)).orThrow() }
-            .apply {
-                expectThat(state).isA<GameState.Bidding>().and {
+            .flatMap { it.execute(StartRoundCommand) }
+            .flatMap { it.execute(PlaceBidCommand(player1, SomeBid.One)) }
+            .flatMap { it.execute(PlaceBidCommand(player2, SomeBid.Zero)) }
+            .peek {
+                expectThat(it.state).isA<GameState.Bidding>().and {
                     get { bids }.getValue(player1).isEqualTo(SomeBid.One)
                     get { bids }.getValue(player2).isEqualTo(SomeBid.Zero)
                 }
-            }.run { execute(StartTrickCommand).orThrow() }
-            .apply {
-                expectThat(state).isA<GameState.TrickTaking>().and {
+            }.flatMap { it.execute(StartTrickCommand) }
+            .peek {
+                expectThat(it.state).isA<GameState.TrickTaking>().and {
                     get { roundNumber }.isEqualTo(RoundNumber.One)
                     get { trickNumber }.isEqualTo(TrickNumber.One)
                 }
-            }
+            }.orThrow()
     }
 }
