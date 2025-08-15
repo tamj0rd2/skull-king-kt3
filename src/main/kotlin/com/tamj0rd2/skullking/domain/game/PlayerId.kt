aa46301de45791ac0f2ab6184507ac55240ab27e
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-08-15 15:42:32 +0100

    Add PlayerId

---
+++ src/main/kotlin/com/tamj0rd2/skullking/domain/game/PlayerId.kt
@@ -0,0 +1,9 @@
+package com.tamj0rd2.skullking.domain.game
+
+import dev.forkhandles.values.StringValueFactory
+import dev.forkhandles.values.Value
+
+@JvmInline
+value class PlayerId(override val value: String) : Value<String> {
+    companion object : StringValueFactory<PlayerId>(::PlayerId)
+}
