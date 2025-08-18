44db279dfa6f15f815cb2de8d9e70ac584e0e78c
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-08-18 21:46:24 +0100

    make players a computed property

--- src/main/kotlin/com/tamj0rd2/skullking/domain/game/Game.kt
+++ src/main/kotlin/com/tamj0rd2/skullking/domain/game/Game.kt
@@ -1,25 +1,23 @@
 package com.tamj0rd2.skullking.domain.game
 
-data class Game
-private constructor(val id: GameId, val players: Set<PlayerId>, val events: List<GameEvent>) {
+data class Game private constructor(val id: GameId, val events: List<GameEvent>) {
     val creator
         get() = (events.first() as GameEvent.GameCreated).createdBy
+
+    val players
+        get() = events.filterIsInstance<GameEvent.PlayerJoined>().map { it.playerId }.toSet()
 
     companion object {
         fun new(id: GameId, createdBy: PlayerId): Game {
             return Game(
                 id = id,
-                players = emptySet(),
                 events = listOf(GameEvent.GameCreated(gameId = id, createdBy = createdBy)),
             )
         }
     }
 
     fun addPlayer(playerId: PlayerId): Game {
-        return copy(
-            players = players + playerId,
-            events = events + GameEvent.PlayerJoined(gameId = id, playerId = playerId),
-        )
+        return copy(events = events + GameEvent.PlayerJoined(gameId = id, playerId = playerId))
     }
 }
 
