0596f6d64031db27ea5957fda07b3e038cafe21b
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-08-26 11:29:24 +0100

    add a way to monitor use cases

--- src/main/kotlin/com/tamj0rd2/skullking/application/UseCases.kt
+++ src/main/kotlin/com/tamj0rd2/skullking/application/UseCases.kt
@@ -2,6 +2,7 @@ package com.tamj0rd2.skullking.application
 
 import com.tamj0rd2.skullking.application.ports.input.CreateGameUseCase
 import com.tamj0rd2.skullking.application.ports.input.JoinGameUseCase
+import com.tamj0rd2.skullking.application.ports.input.UseCase
 import com.tamj0rd2.skullking.application.ports.input.ViewGamesUseCase
 import com.tamj0rd2.skullking.application.services.CreateGameService
 import com.tamj0rd2.skullking.application.services.JoinGameService
@@ -31,5 +32,21 @@ data class UseCases(
                     ),
             )
         }
+    }
+
+    fun monitorWith(
+        inputMonitor: (input: Any) -> Unit = { _ -> },
+        outputMonitor: (output: Any?) -> Unit = { _ -> },
+    ): UseCases {
+        fun <I : Any, O : Any> UseCase<I, O>.decorate(): (I) -> O = { input: I ->
+            inputMonitor(input)
+            execute(input).also { outputMonitor(it) }
+        }
+
+        return UseCases(
+            createGameUseCase = createGameUseCase.decorate(),
+            viewGamesUseCase = viewGamesUseCase.decorate(),
+            joinGameUseCase = joinGameUseCase.decorate(),
+        )
     }
 }
--- src/main/kotlin/com/tamj0rd2/skullking/application/ports/input/UseCase.kt
+++ src/main/kotlin/com/tamj0rd2/skullking/application/ports/input/UseCase.kt
@@ -1,5 +1,5 @@
 package com.tamj0rd2.skullking.application.ports.input
 
-fun interface UseCase<I, O> {
+fun interface UseCase<in I : Any, out O : Any> {
     fun execute(input: I): O
 }
